14a0f31529a15cf9a94c8377f19c214a85e501e7
Shivam Chaudhary <shivamchaudhary310899@gmail.com>
2023-05-16 18:50:17 +0530

    finding missing number

--- 2-B.js
+++ 2-B.js
@@ -1,7 +1,7 @@
 function findMissingNumber(nums) {
-    const n = nums.length + 1; // Add 1 to account for the missing number
-    const expectedSum = (n * (n + 1)) / 2; // Sum of integers from 1 to n
-    const actualSum = nums.reduce((sum, num) => sum + num, 0); // Sum of elements in input array
-    return expectedSum - actualSum; // Difference is the missing number
+    const n = nums.length + 1; 
+    const expectedSum = (n * (n + 1)) / 2; 
+    const actualSum = nums.reduce((sum, num) => sum + num, 0); 
+    return expectedSum - actualSum; 
   }
   
